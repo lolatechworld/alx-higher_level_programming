701b88ed8ab4b8db992c1d8bb6425520b284a2cf
Oluwadamilola Owolabi <owolabioluwadamilola1@gmail.com>
2024-01-08 09:22:18 -0800

    palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,62 +1,70 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
-* reverse_list - Reverses a linked list.
-* @head: Pointer to the head of the linked list.
-* Return: Pointer to the new head of the reversed list.
-*/
-listint_t *reverse_list(listint_t *head)
+ * reverse_listint - reverses a linked list
+ * @head: pointer to the first node in the list
+ * Return: pointer to the first node in the new list
+ */
+void reverse_listint(listint_t **head)
 {
-listint_t *prev = NULL;
-listint_t *current = head;
-listint_t *next;
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
 
-while (current != NULL)
-{
-next = current->next;
-current->next = prev;
-prev = current;
-current = next;
-}
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
 
-return (prev);
+	*head = prev;
 }
 
 /**
-* is_palindrome - Checks if a singly linked list is a palindrome.
-* @head: Pointer to the head of the linked list.
-* Return: 1 if it is a palindrome, 0 otherwise.
-*/
+ * is_palindrome - checks if a linked list is a palindrome
+ * @head: double pointer to the linked list
+ * Return: 1 if it is, 0 if not
+ */
 int is_palindrome(listint_t **head)
 {
-if (*head == NULL || (*head)->next == NULL)
-return (1);
+	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
 
-listint_t *slow = *head;
-listint_t *fast = *head;
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
 
-while (fast != NULL && fast->next != NULL)
-{
-slow = slow->next;
-fast = fast->next->next;
-}
+	while (1)
+	{
+		fast = fast->next->next;
+		if (!fast)
+		{
+			dup = slow->next;
+			break;
+		}
+		if (!fast->next)
+		{
+			dup = slow->next->next;
+			break;
+		}
+		slow = slow->next;
+	}
 
-listint_t *second_half = reverse_list(slow);
-listint_t *first_half = *head;
+	reverse_listint(&dup);
 
-while (second_half != NULL)
-{
-if (first_half->n != second_half->n)
-{
-reverse_list(second_half);
-return (0);
-}
-first_half = first_half->next;
-second_half = second_half->next;
-}
+	while (dup && temp)
+	{
+		if (temp->n == dup->n)
+		{
+			dup = dup->next;
+			temp = temp->next;
+		}
+		else
+			return (0);
+	}
 
-reverse_list(second_half);
-return (1);
+	if (!dup)
+		return (1);
+
+	return (0);
 }
